7c92400908ffecabeecb8f2f898714f7da855cfd
Chris fredo <programmingwithchrisfredo20@gmail.com>
2023-09-27 16:11:41 +0100

    ATM-app
    Initial/All commit

---
+++ Messages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM1
+{
+    public class Messages
+    {
+        const string userMessage = "Welcome, please insert your card";
+        const string successful = "Card inserted successfully";
+        const string insufficient = "Insufficient balance";
+        const string goodBye = "Thank you for banking with us";
+        const string selectT = "Select your transaction";
+        const string withdrawal = "w for withdrawal";
+        const string deposit = "d for transfer";
+        const string balanceEnquiry = "i for balance enquiry";
+        const string amount1 = "Enter the amount you want to withdraw; $";
+        const string amount2 = "Enter the amount you want to deposit; $";
+
+
+        public static void MessageMethod(short? MessageIndex, string? result = null)
+        {
+            switch (MessageIndex)
+            {
+                case 0:
+                    Console.WriteLine(userMessage);
+                    break;
+                case 1:
+                    Console.WriteLine(successful);
+                    break;
+                case 2:
+                    Console.WriteLine(insufficient);
+                    break;
+                case 3:
+                    Console.WriteLine(goodBye);
+                    break;
+                case 4:
+                    Console.WriteLine(selectT);
+                    break;
+                case 5:
+                    Console.WriteLine(withdrawal);
+                    break;
+                case 6:
+                    Console.WriteLine(deposit);
+                    break;
+                case 7:
+                    Console.WriteLine(balanceEnquiry);
+                    break;
+                case 8:
+                    Console.WriteLine(amount1);
+                    break;
+                case 9:
+                    Console.WriteLine(amount2);
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using ATM1;
+
+namespace ATMmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = 5000;
+            double withdraw;
+            double deposit;
+            double nBalance;
+            string balanceEnquiry;
+            string select;
+            string input;
+            
+
+
+
+            Messages.MessageMethod(0);
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.WriteLine();
+
+            Messages.MessageMethod(1);
+            Console.WriteLine();
+            Messages.MessageMethod(4);
+            Messages.MessageMethod(5);
+            Messages.MessageMethod(6);
+            Messages.MessageMethod(7);
+            Console.WriteLine();
+            Console.Write("Select here;");
+
+            select = Console.ReadLine();
+            Console.WriteLine();
+
+            if (select == ("w") || select == ("W"))
+            {
+                Messages.MessageMethod(8);
+                withdraw = double.Parse(Console.ReadLine());
+                nBalance = balance -= withdraw;
+                Console.Write("New Balance: $ " + balance);
+            }
+            else if (select == ("d") || select == ("D")) 
+            {
+                Messages.MessageMethod(9);
+                deposit = double.Parse(Console.ReadLine());
+                nBalance = balance += deposit;
+                Console.WriteLine("New Balance: $ " + balance);
+            }
+            else if (select == ("i") || select == ("I"))
+            {
+                Messages.MessageMethod(7);
+                balanceEnquiry = Console.ReadLine();
+                Console.Write("Your balance is: " + balance);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Sorry, there was an error, try again!!!");
+            }
+        }
+
+    }
+}
